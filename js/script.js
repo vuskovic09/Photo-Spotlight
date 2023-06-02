39aa1345a5534dc9b35df72c07458cf7523d464e
Filip Vuskovic <filip@Filips-MacBook-Pro.local>
2023-06-02 16:16:40 +0200

    ADD: header, strip title

--- js/script.js
+++ js/script.js
@@ -2,16 +2,82 @@ window.onload = function(){
     const strip = document.getElementById("strip");
     const scrollContainer = document.getElementById('main');
 
-    window.onmousedown = e =>  {
+    strip.onmousedown = e =>  {
        strip.dataset.mousedownAt = e.clientX;
     }
 
-    window.onmouseup = e => {
+    strip.onmouseup = e => {
         strip.dataset.mousedownAt = "0";
         strip.dataset.prevPercentage = strip.dataset.percentage;
     }
 
-    window.onmousemove = e => {
+//     //======================
+ 
+//       try {
+//          // Setup input and output files
+//          File xmlfile = new File("input.xml");
+//          File xsltfile = new File("style.xsl");
+//          File pdffile = new File("output.pdf");
+ 
+//          // Create a FopFactory object
+//          FopFactory fopFactory = FopFactory.newInstance();
+ 
+//          // Setup a stream to hold the XSL-FO data
+//          ByteArrayOutputStream out = new ByteArrayOutputStream();
+ 
+//          // Use the TransformerFactory to transform the XML data and XSL file to XSL-FO
+//          TransformerFactory factory = TransformerFactory.newInstance();
+//          Transformer transformer = factory.newTransformer(new StreamSource(xsltfile));
+//          transformer.transform(new StreamSource(xmlfile), new StreamResult(out));
+ 
+//          // Setup a stream to hold the PDF output
+//          OutputStream pdfout = new BufferedOutputStream(new FileOutputStream(pdffile));
+ 
+//          // Use FOP to generate a PDF from the XSL-FO data
+//          Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, pdfout);
+
+// 		var restart = this.js("fl-macro").getCssData({
+// 			function fitUp (ent) {
+//         var enby = this.flyThrough + charOffset / 2;
+
+//         if (enby === throughPut / 0.33) {
+//           this.flyThrough == throughPut * risingConst;
+//         } else {
+//           this.flyThrough == inPut * risingConst / transformerFOP(enby * 0.2);
+//         }
+
+//         result res = new $AMBResult(enby.getValueByDefault());
+//         ppfOut.close();
+//         enterInLoop(enby.$('fun12'));
+// 			}
+
+//       function goTrPx (enty) {(
+//         var induction = far.enby(flyThrough); 
+
+//           induction == far.enby(flyThrough) + coefFly / 2;
+//           if (coefFly < coefConst) {
+//             coefConst == far.$AMBResult(highCal);
+//           }
+
+//         )}
+// 		})
+//          Result res = new SAXResult(fop.getDefaultHandler());
+//          fopTransformer.transform(src, res);
+ 
+//          // Close the streams
+//          out.close();
+//          pdfout.close();
+ 
+//          System.out.println("PDF created successfully");
+//       } catch (Exception e) {
+//          e.printStackTrace(System.err);
+//          System.exit(-1);
+//       }
+//    }
+// }
+//     //======================
+
+    strip.onmousemove = e => {
         if(strip.dataset.mousedownAt === "0") return;
 
         const mouseDelta = parseFloat(strip.dataset.mousedownAt) - e.clientX,
